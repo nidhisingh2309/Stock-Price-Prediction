839580f3d219b6e70552224180b3ee3ab5f4c0a9
Nidhi Singh <103565450+nidhisingh2309@users.noreply.github.com>
2024-10-15 00:52:00 +0530

    Add files via upload

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+
+
+__author__ = "Nidhi Singh"
+__copyright__ = "Nidhi Singh 2024"
+__version__ = "2.0.0"
+__license__ = "MIT"
+
+import warnings
+warnings.filterwarnings("ignore") # ignore messy numpy warnings
